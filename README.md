2c4ea7a9ff05c200ea851e5b75711c216c609c40
Jakub Hrozek <jakub.hrozek@posteo.se>
2016-07-20 10:53:39 +0200

    Fixes and readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # nssbench
+This is a simple C program and associated systemtap script that measures
+the time spent in a libc NSS operation and in the particular operations
+in the files and sss NSS modules.
+
+## build
+just `make`
+
+## usage
+You need systemtap installed along with kernel-headers and
+kernel-devel. Then, in one window, run the nssbench program and in another
+window, run the systemtap script.
+
+The nssbench program accepts two, optionally three positional arguments
+- the NSS function to run, the input of the function and optionally the
+number of repetitions. For example:
+```bash
+./nssbench getpwnam jhrozek 100
+```
+
+The systemtap script is run without any arguments:
+```bash
+sudo stap ./nssbench.stp
+```
+
+You don't need to restart the script between successive runs of nssbench,
+the variables are cleared automatically. The script should produce output
+such as:
+```bash
+sudo stap ./nssbench.stp
+nss operation getpwnam(jhrozek) took 2654 us
+_nss_files_getpwnam cnt:100 avg:15 min:13 max:53 sum:1548 us
+_nss_sss_getpwnam cnt:100 avg:3 min:2 max:45 sum:305 us
+```
--- nssbench.c
+++ nssbench.c
@@ -4,6 +4,7 @@
 #include <strings.h>
 #include <errno.h>
 #include <pwd.h>
+#include <grp.h>
 
 struct nss_fn_name_map {
     const char *fname;
@@ -12,22 +13,47 @@ struct nss_fn_name_map {
 
 enum nss_funcs {
     NB_GETPWNAM,
+    NB_GETGRNAM,
+    NB_GETPWUID,
+    NB_GETGRGID,
+    NB_INITGR,
 };
 
+/* This function is separate and the enum values are used to allow systemtap
+ * to only measure the single function. I wasn't able to figure out how to
+ * run systemtap for either getpwnam() or getpwnam_r() directly w/o a wrapper..
+ */
 static void nss_wrapper(enum nss_funcs nb_fn,
                         long long id_arg,
                         const char *str_arg)
 {
     struct passwd *pwd = NULL;
+    struct group *grp = NULL;
+    int ret;
 
     switch (nb_fn) {
     case NB_GETPWNAM:
         pwd = getpwnam(str_arg);
         break;
+    case NB_GETGRNAM:
+        grp = getgrnam(str_arg);
+        break;
+    case NB_GETPWUID:
+        pwd = getpwuid(id_arg);
+        break;
+    case NB_GETGRGID:
+        grp = getgrgid(id_arg);
+        break;
+    case NB_INITGR:
+        ret = initgroups(str_arg, 0);
+        break;
+    default:
+        break;
     }
 }
 
 static void nss_wrapper_loop(enum nss_funcs nb_fn,
+                             const char *fn_name, /* for systemtap.. */
                              long long id_arg,
                              const char *str_arg,
                              size_t count)
@@ -57,7 +83,15 @@ int main(int argc, const char *argv[])
 
     fname = argv[1];
     if (strcasecmp(fname, "getpwnam") == 0) {
-        nss_wrapper_loop(NB_GETPWNAM, -1, argv[2], count);
+        nss_wrapper_loop(NB_GETPWNAM, fname, -1, argv[2], count);
+    } else if (strcasecmp(fname, "getgrnam") == 0) {
+        nss_wrapper_loop(NB_GETGRNAM, fname, -1, argv[2], count);
+    } else if (strcasecmp(fname, "getpwuid") == 0) {
+        nss_wrapper_loop(NB_GETPWUID, fname, atoi(argv[2]), argv[2], count);
+    } else if (strcasecmp(fname, "getgrgid") == 0) {
+        nss_wrapper_loop(NB_GETGRGID, fname, atoi(argv[2]), argv[2], count);
+    } else if (strcasecmp(fname, "initgroups") == 0) {
+        nss_wrapper_loop(NB_INITGR, fname, -1, argv[2], count);
     } else {
         fprintf(stderr, "nssbench does not handle %s\n", fname);
     }
